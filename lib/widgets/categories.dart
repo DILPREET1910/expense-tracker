87b52491b722aaa4cc8ff579a9867cde27aaa138
DILPREET1910 <dsahney1910@gmail.com>
2023-04-25 21:03:37 +0530

    categories card - rounded edges,icons,name text, amount

--- lib/widgets/categories.dart
+++ lib/widgets/categories.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Categories extends StatefulWidget {
   String name;
@@ -19,14 +20,36 @@ class CategoriesCard extends State<Categories> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.white60,
-
-      child: Row(
-        children: [
-          Text(widget.name),
-          Text(widget.amount),
-          Text(widget.icon),
-        ],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(19)),
+      color: Colors.blueGrey[100],
+      elevation: 5,
+      shadowColor: Colors.black12,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 7, 20, 7),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //icon #start
+            const CircleAvatar(
+              radius: 20,
+              backgroundColor: Colors.black12,
+              child: Icon(
+                Icons.money,
+                size: 30,
+                color: Colors.black,
+              ),
+            ),
+            //icon #end
+            Text(
+              widget.name,
+              style: GoogleFonts.balooBhai2(
+                fontSize: 20,
+                letterSpacing: 5,
+              ),
+            ),
+            Text(widget.amount),
+          ],
+        ),
       ),
     );
   }

9e1484f181dc68dfee61e0303f68b1c678b2ae01
DILPREET1910 <dsahney1910@gmail.com>
2023-05-12 22:23:10 +0530

    fixed render flex overflow

--- lib/widgets/dataEntries.dart
+++ lib/widgets/dataEntries.dart
@@ -23,80 +23,81 @@ class _DataEntriesState extends State<DataEntries> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(20, 7, 20, 7),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          //START: Date
-          Text(
-            widget.date,
-            style: GoogleFonts.balooBhai2(
-              fontSize: 20,
-              letterSpacing: 5,
-            ),
-          ),
-          //END: Date
-          //START: Categories
-          Text(
-            widget.category,
-            style: GoogleFonts.balooBhai2(
-              fontSize: 20,
-              letterSpacing: 5,
-            ),
-          ),
-          //END: Categories
-          //START: Description
-          Text(
-            widget.description,
-            style: GoogleFonts.balooBhai2(
-              fontSize: 20,
-              letterSpacing: 0,
-            ),
-          ),
-          //amount + pop up menu #start
-          Row(
-            children: [
-              //category amount #start
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(widget.amount),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //START: Date
+            Text(
+              widget.date,
+              style: GoogleFonts.balooBhai2(
+                fontSize: 20,
+                letterSpacing: 5,
               ),
-              //category amount #end
-              //pop up menu button #start
-              PopupMenuButton<int>(
-                itemBuilder: (context) => [
-                  //pop up menu 1st item
-                  const PopupMenuItem(
-                    value: 1,
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Icon(Icons.edit),
-                        ),
-                        Text('Edit'),
-                      ],
-                    ),
+            ),
+            //END: Date
+            //START: Categories
+            Text(
+              widget.category,
+              style: GoogleFonts.balooBhai2(
+                fontSize: 20,
+                letterSpacing: 5,
+              ),
+            ),
+            //END: Categories
+            //START: Description
+            Text(
+              widget.description,
+              style: GoogleFonts.balooBhai2(
+                fontSize: 20,
+                letterSpacing: 0,
+              ),
+            ),
+            //END: Description
+            //START: Amount
+            Text(
+              widget.amount,
+              style: GoogleFonts.balooBhai2(
+                fontSize: 20,
+                letterSpacing: 4,
+              ),
+            ),
+            //END: Amount
+            //START: pop up menu
+            PopupMenuButton<int>(
+              itemBuilder: (context) => [
+                //pop up menu 1st item
+                const PopupMenuItem(
+                  value: 1,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Icon(Icons.edit),
+                      ),
+                      Text('Edit'),
+                    ],
                   ),
-                  //pop up menu 2nd item
-                  const PopupMenuItem(
-                    value: 2,
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Icon(Icons.delete),
-                        ),
-                        Text('Delete'),
-                      ],
-                    ),
-                  )
-                ],
-              )
-              //pop up menu button #end
-            ],
-          )
-          //amount + pop up menu #start
-        ],
+                ),
+                //pop up menu 2nd item
+                const PopupMenuItem(
+                  value: 2,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Icon(Icons.delete),
+                      ),
+                      Text('Delete'),
+                    ],
+                  ),
+                )
+              ],
+            )
+            //END: pop up menu
+          ],
+        ),
       ),
     );
   }

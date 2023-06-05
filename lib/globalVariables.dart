1b3d305b94055cc390c3d2b5efb3461f7ba82c1e
DILPREET1910 <dsahney1910@gmail.com>
2023-06-05 18:28:29 +0530

    starting clean
    removed all lib files

--- lib/globalVariables.dart
+++
@@ -1,5 +0,0 @@
-//date formatter import
-import 'package:intl/intl.dart';
-
-String dateText = DateFormat('dd-MMM-yyyy').format(DateTime.now());
-List<String> list = ['mess', 'non mess food', 'travelling', 'VIT stuff', 'stationary','household stuff'];
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,5 @@
-//import pages
-import 'package:expense_tracker/api/Google%20Sheets/user_sheet_api.dart';
-import 'package:expense_tracker/pages/Data.dart';
-import 'package:expense_tracker/pages/Dashboard.dart';
-import 'package:expense_tracker/pages/Settings.dart';
-
 import 'package:flutter/material.dart';
 
-void main() async {
-  // check for independent of main branch
-  // github conflict with new branch - responsive
-  WidgetsFlutterBinding.ensureInitialized();
-  await UserSheetsApi.init();
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => Data(),
-      '/dashboard': (context) => Dashboard(),
-      '/settings': (context) => Settings(),
-    },
-  ));
+void main() {
+  runApp(MaterialApp());
 }

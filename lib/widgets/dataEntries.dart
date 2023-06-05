34e49e250cb1c50df8ea2d391db84025c2161d64
DILPREET1910 <dsahney1910@gmail.com>
2023-06-05 17:43:06 +0530

    epoch to normal date on data entries

--- lib/widgets/dataEntries.dart
+++ lib/widgets/dataEntries.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+//importing date formatter
+import 'package:intl/intl.dart';
+
 class DataEntries {
+  //epoch to normal date converter function
+  static var epoch = DateTime(1899, 12, 30);
+
+  static String noEpoch(String date) {
+    var currentDate = epoch.add(Duration(days: int.parse(date)));
+    var returnThis = DateFormat('dd-MMM-yyyy').format(currentDate);
+    return returnThis;
+  }
+
   static DataRow dataEntries(
       String date, String category, String description, String amount) {
     return DataRow(cells: [
-      DataCell(Text(date)),
+      DataCell(Text(noEpoch(date))),
       DataCell(Text(category)),
       DataCell(Text(description)),
       DataCell(Text(amount)),

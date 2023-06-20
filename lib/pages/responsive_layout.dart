991762db68d7d64570ab07d71e19474813b04b88
DILPREET1910 <dsahney1910@gmail.com>
2023-06-20 13:27:53 +0530

    initial tablet commit

--- lib/pages/responsive_layout.dart
+++ lib/pages/responsive_layout.dart
@@ -11,7 +11,7 @@ class ResponsiveLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-      if (constraints.maxWidth < 500) {
+      if (constraints.maxWidth < 750) {
         return mobile;
       } else if (constraints.maxWidth < 1100) {
         return tablet;
--- lib/pages/tablet.dart
+++ lib/pages/tablet.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/drawer.dart';
+import 'dataEntry.dart';
+
 class Tablet extends StatefulWidget {
   const Tablet({Key? key}) : super(key: key);
 
@@ -10,8 +13,17 @@ class Tablet extends StatefulWidget {
 class _TabletState extends State<Tablet> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey,
-    );
+    return SafeArea(
+        child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            //prevents overflow error when keyboard pops up in mobile devices
+            backgroundColor: Colors.grey[400],
+            body: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(flex: 1, child: WidgetsDrawer()),
+                Expanded(flex: 2, child: WidgetsDataEntry()),
+              ],
+            )));
   }
 }

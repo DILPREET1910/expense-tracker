eabae6949bcc8233a4ce46f8ab083855a663493b
DILPREET1910 <dsahney1910@gmail.com>
2023-09-14 15:14:37 +0530

    text ellipsis on pie chart label

--- lib/services/graph/pieChartSectionData.dart
+++ lib/services/graph/pieChartSectionData.dart
@@ -16,7 +16,9 @@ List<PieChartSectionData> widgetsPieChartSelectionData(
       (index) => PieChartSectionData(
           color: touchIndex == index ? pieColorsFocus[index] : pieColors[index],
           value: nonZeroData[index][1],
-          title: nonZeroData[index][0],
+          title: nonZeroData[index][0].length <= 12
+              ? nonZeroData[index][0]
+              : "${nonZeroData[index][0].substring(0, 12)}...",
           titleStyle: touchIndex == index
               ? GoogleFonts.ubuntu(
                   fontSize: 30,
@@ -26,7 +28,7 @@ List<PieChartSectionData> widgetsPieChartSelectionData(
               : GoogleFonts.ubuntu(
                   fontSize: 20,
                   fontWeight: FontWeight.w500,
-                  letterSpacing: 1,
+                  letterSpacing: 0,
                   color: Colors.black54),
           radius: touchIndex == index ? 180 : 150));
 }
